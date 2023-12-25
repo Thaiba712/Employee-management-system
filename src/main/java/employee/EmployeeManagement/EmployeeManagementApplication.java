17c4efc96d91a386aa26b19aebc3980ac40a096e
thaiba712 <fathimathahiba@gmail.com>
2023-12-25 11:15:55 +0530

    comment added

--- src/main/java/employee/EmployeeManagement/EmployeeManagementApplication.java
+++ src/main/java/employee/EmployeeManagement/EmployeeManagementApplication.java
@@ -2,7 +2,9 @@ package employee.EmployeeManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController //added
 @SpringBootApplication
 public class EmployeeManagementApplication {
 
